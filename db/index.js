07e029bad782b34fbb43ee28b3dedac2d2a90031
Callie Cassan <contactccassan@gmail.com>
2023-04-12 11:29:02 -0500

    coming back to this

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 // Require the Client constructor from the pg package
-
+const { Client } = require('pg');
 // Create a constant, CONNECTION_STRING, from either process.env.DATABASE_URL or postgres://localhost:5432/phenomena-dev
-
 // Create the client using new Client(CONNECTION_STRING)
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/phenomena-dev');
 // Do not connect to the client in this file!
 
 /**
@@ -20,11 +20,19 @@
 async function getOpenReports() {
   try {
     // first load all of the reports which are open
-    
+    const { rows: reports } = await client.query(`
+      SELECT * 
+      FROM reports 
+      WHERE reports."isOpen"=true;
+    `)
 
     // then load the comments only for those reports, using a
     // WHERE "reportId" IN () clause
-
+    const { rows: comments } = await client.query(`
+      SELECT * 
+      FROM comments 
+      WHERE "reportId" IN (${ reports.map((report) => report.id).join(', ') })
+    `)
     
     // then, build two new properties on each report:
     // .comments for the comments which go with it
@@ -32,10 +40,15 @@ async function getOpenReports() {
     // .isExpired if the expiration date is before now
     //    you can use Date.parse(report.expirationDate) < new Date()
     // also, remove the password from all reports
+    reports.forEach((report) => {
+      delete report.password;
+      report.isExpired = report.expirationDate < new Date();
+      report.comments = comments.filter((comment) => comment.reportId === report.id);
+    });
 
 
     // finally, return the reports
-  
+  return reports;
 
   } catch (error) {
     throw error;
@@ -55,19 +68,22 @@ async function getOpenReports() {
  */
 async function createReport(reportFields) {
   // Get all of the fields from the passed in object
-
+const { title, location, description, password } = reportFields;
 
   try {
     // insert the correct fields into the reports table
     // remember to return the new row from the query
-    
+    const { rows } = await client.query(`
+      INSERT INTO reports(title, location, description, password)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [title, location, description, password]);
 
     // remove the password from the returned row
-    
+   delete rows[0].password;
 
     // return the new report
-    
-
+ return rows[0];
   } catch (error) {
     throw error;
   }
@@ -90,10 +106,14 @@ async function createReport(reportFields) {
 async function _getReport(reportId) {
   try {
     // SELECT the report with id equal to reportId
-    
+    const { rows: [report] } =  await client.query(`
+      SELECT *
+      FROM reports 
+      WHERE id=${reportId}
+    `)
 
     // return the report
-    
+    return report;
 
   } catch (error) {
     throw error;
@@ -112,27 +132,35 @@ async function _getReport(reportId) {
 async function closeReport(reportId, password) {
   try {
     // First, actually grab the report with that id
-    
-
+    const report = await _getReport(reportId);
     // If it doesn't exist, throw an error with a useful message
-    
-  
-    // If the passwords don't match, throw an error
-    
+    if(!report) {
+      throw new Error('Report does not exist with that id');
+    };
 
+    // If the passwords don't match, throw an error
+    if (report.password !== password) {
+      throw new Error('Password incorrect for this report, please try again');
+    };
     // If it has already been closed, throw an error with a useful message
-    
+    if (!report.isOpen) {
+      throw new Error ('This report has already been closed');
+    };
 
     // Finally, update the report if there are no failures, as above
-    
+    await client.query(`
+    UPDATE reports
+    SET "isOpen"=false
+    WHERE id=$1;
+    `, [reportId]);
 
     // Return a message stating that the report has been closed
-    
-
-  } catch (error) {
+    return {message: 'Report successfully closed!'}
+  }catch(error) {
     throw error;
   }
 }
+
 
 /**
  * Comment Related Methods
@@ -178,3 +206,11 @@ async function createReportComment(reportId, commentFields) {
 }
 
 // export the client and all database functions below
+module.exports = {
+  client, 
+  createReport,
+  getOpenReports, 
+  _getReport, 
+  closeReport, 
+  createReportComment 
+}
--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-// Use the dotenv package, to create environment variables
+// // Use the dotenv package, to create environment variables
+// require('dotenv').config(); 
 
-// Create a constant variable, PORT, based on what's in process.env.PORT or fallback to 3000
+// // Create a constant variable, PORT, based on what's in process.env.PORT or fallback to 3000
+const PORT = process.env.PORT || 3000;
 
-// Import express, and create a server
+// // Import express, and create a server
+const express = require('express');
+const app = express();
 
-// Require morgan and body-parser middleware
+// // Require morgan and body-parser middleware
+// const morgan = require('morgan');
+// const bodyParser = require('body-parser');
 
-// Have the server use morgan with setting 'dev'
+// // Have the server use morgan with setting 'dev'
+// app.use(morgan('dev'));
 
-// Import cors 
-// Have the server use cors()
-
+// // Import cors 
+// const cors =require('cors');
+// // Have the server use cors()
+// app.use(cors());
 // Have the server use bodyParser.json()
 
 // Have the server use your api router with prefix '/api'
 
 // Import the client from your db/index.js
+const { client } = require('./db/index.js'); 
 
 // Create custom 404 handler that sets the status code to 404.
-
+app.use('*', (req, res, send))
 // Create custom error handling that sets the status code to 500
 // and returns the error as an object
 
 
 // Start the server listening on port PORT
 // On success, connect to the database
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+});
